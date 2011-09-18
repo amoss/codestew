e17df1454a095c4bbbbce7b5a468beb955506fc3
Andrew Moss <amoss@Mossbook.local>
2011-09-18 21:45:36 +0200

    First stab at a modulo scheduler in the ARM backend.
    Modulo is the wrong terminology; this isn't allocating registers
    according to a fixed period across loop iterations. This is executing
    a simulation of the graph and using the live/dead state to allocate
    the registers.
    
    The debug looks like it works, but the graphviz output has no strings
    in the registers so something goes wonky...

--- arm.cc
+++ arm.cc
@@ -1,3 +1,4 @@
+#include <map>
 #include <stdio.h>
 #include <stdlib.h>
 #include "arm.h"
@@ -27,9 +28,10 @@ Projection *p = newValSplit(block,W);
 }
 
 char const* regNames[] = {
-  "r0", "r1", "r2", "r3", "r4", "r5", "r6", "r7", "r8", "r9"
+  "r0", "r1", "r2", "r3", "r4", "r5", "r6", "r7", "r8", "r9",
+  "r10", "r11" // , "r12"
 };
-int numRegs = 10;
+int numRegs = 12;
 std::string ArmMachine::outCodeworks(Block *block)
 {
   printf("OUTPUT\n");
@@ -68,9 +70,106 @@ static bool trivial(Allocation *regAlloc)
   return false;
 }
 
-static bool modulo(Allocation *regAlloc)
+typedef std::map<Value*,char const*> LiveSet;
+typedef std::set<char const*> FreePool;
+
+static bool instReady(Instruction *inst, LiveSet live)
 {
-  return false;
+  for(int i=0; i<inst->inputs.size(); i++)
+  {
+    LiveSet::iterator it = live.find( inst->inputs[i] );
+    if( it == live.end() )
+      return false;
+  }
+  return true;
+}
+
+static void dumpState(LiveSet live, FreePool pool)
+{
+LiveSet::iterator lit;
+FreePool::iterator pit;
+  for(lit=live.begin(); lit!=live.end(); lit++)
+      printf("%s:%s ", (*lit).first->type->repr().c_str(), (*lit).second);
+  printf("| ");
+  for(pit=pool.begin(); pit!=pool.end(); pit++)
+    printf("%s ", *pit);
+  printf("\n");
+}
+
+static bool checkKill(Instruction *inst, Value *v, std::set<Instruction*> done)
+{
+  for(int i=0; i<v->uses.size(); i++)
+  {
+    if( done.find(v->uses[i]) == done.end() )
+      return false;
+  }
+  return true;
+}
+
+static bool allocReg(Value *v, LiveSet &live, FreePool &pool)
+{
+FreePool::iterator it;
+  if(pool.size()==0)
+    return false;
+  it = pool.begin();
+  live[v] = *it;
+  pool.erase(it);
+  return true;
+}
+
+static bool modulo(Allocation *regAlloc, std::vector<Instruction*> order)
+{
+LiveSet live;
+FreePool pool;
+std::set<Instruction*> done;
+  for(int i=0; i<regAlloc->numInputs(); i++)
+    live[regAlloc->getInput(i)] = regNames[i];
+  for(int i=regAlloc->numInputs(); i<numRegs; i++)
+    pool.insert(regNames[i]);
+
+  while(done.size() < order.size() )
+  {
+    dumpState(live,pool);
+    for(int i=0; i<order.size(); i++)
+    {
+      Instruction *inst = order[i];
+      std::set<Instruction*>::iterator it=done.find(inst);
+      if( it!=done.end() )
+        continue;
+      if( instReady(inst,live) )
+      {
+        // Atomic assumption: work out which registers are consumed by this instruction
+        // (this is the last use in this ordering) and free those registers before 
+        // allocating targets.
+        printf("Proc %s ready\n", inst->repr().c_str());
+        done.insert(inst);
+        for(int i=0; i<inst->inputs.size(); i++)
+          if( checkKill(inst, inst->inputs[i], done) )
+          {
+            LiveSet::iterator it = live.find(inst->inputs[i]);
+            char const *reg = (*it).second;
+            printf("Killed %llu, freed %s\n", inst->inputs[i]->ref, reg);
+            live.erase(it); 
+            pool.insert( reg );
+          }
+          else
+            printf("Still live %llu\n", inst->inputs[i]);
+        for(int i=0; i<inst->outputs.size(); i++)
+        {
+          if(!allocReg(inst->outputs[i], live, pool))
+          {
+            printf("Can't allocate for %s (%zu)\n", inst->repr().c_str(), pool.size());
+            return false;
+          }
+        }
+      }
+      else
+        printf("Proc %s waiting\n", inst->repr().c_str());
+    }
+  }
+  dumpState(live,pool);
+
+  return true;
 }
 
 Allocation *ArmMachine::allocate(Block *block)
@@ -91,7 +190,8 @@ Allocation *regAlloc = new Allocation(block);
 
   if( trivial(regAlloc) )
     return regAlloc; 
-  if( modulo(regAlloc) )
+std::vector<Instruction*> order = regAlloc->topSort();
+  if( modulo(regAlloc,order) )
     return regAlloc;
   printf("Modulo failed\n");
   exit(-1);
